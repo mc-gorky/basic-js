c1888448daaace9d53716e4d6a86e68ab8e6eb3c
Serg <mcgorky@gmail.com>
2020-02-19 22:06:22 +0300

    feat: solve carbon-dating and simple chain

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,17 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (!sampleActivity || typeof sampleActivity != 'string') {
+    return false;
+  }
+  
+  let sample = parseFloat(sampleActivity);
+ 
+ 
+    if (isNaN(sample) || sample > MODERN_ACTIVITY || sampleActivity <= 0) {
+    return false;
+  }
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sample) / (0.693 / HALF_LIFE_PERIOD));
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,32 @@
 const chainMaker = {
+  
+  chain: [],
+  
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if(!position || !Number.isInteger(position) || position < 0 || position > this.chain.length){
+      this.chain = [];
+      throw new Error();
+     
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let chainString = this.chain.join('~~');
+    this.chain = [];
+    return chainString;
+    
   }
 };
 
